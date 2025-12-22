fe2d99e566b9baeb5a38e64c0afddda3478f2880
Irja <luca.sanna2222@gmail.com>
2025-12-22 13:09:20 +0100

    relazione conclusa!

--- analysis.py
+++ analysis.py
@@ -175,70 +175,6 @@ def compute_peaks():
 
 
 ######################################################
-# Plotting for report.
-######################################################
-def plot_allofit(datasets, savename = ""):
-    my_dpi = 192
-    tnrfont = {'fontname':'Times New Roman'}
-    plt.rcParams['axes.axisbelow'] = True
-    plt.rcParams['mathtext.default'] = 'regular'
-    plt.rcParams["legend.frameon"] = True
-    plt.rcParams["legend.fancybox"] = False
-    zorders = np.linspace(1, 6, 6)
-    colors = ["orangered", "forestgreen", "lightseagreen", "darkorchid", "chartreuse", "crimson"]
-    labels = ["sample 1", "sample 2", "sample 3", "sample 4", "sample 5", "sample 6"]
-
-    fig, ax = plt.subplots(num = "hist",
-                            figsize = (4000/my_dpi, 2000/my_dpi),
-                            dpi = my_dpi)
-    for dataset in range(len(datasets)):
-        plt.plot(datasets[dataset][:, 0], datasets[dataset][:, 1],
-                color = colors[dataset],
-                linewidth = 1.5,
-                label = labels[dataset],
-                zorder = zorders[dataset])
-    plt.text(0.75, 0.85,
-            "GID XRD diffractogram\nof sapphire thin films",
-            **tnrfont,
-            fontsize = 52,
-            horizontalalignment = 'center',
-            verticalalignment = 'center',
-            transform = ax.transAxes,
-            bbox = dict(facecolor = 'white', alpha = 1))
-    label_fontsize = 44
-    ax.set_xlabel("$2 \\vartheta$ ($deg$)",
-                fontsize = label_fontsize,
-                **tnrfont)
-    ax.set_ylabel("counts",
-                fontsize = label_fontsize,
-                **tnrfont)
-    x_min, x_max = 10, np.max(datasets[0][:, 0])
-    x_ticks = np.linspace(x_min, x_max, 10)
-    x_ticks_names = []
-    for tick in x_ticks:
-        x_ticks_names.append(str(int(tick)))
-    ax.set_xlim(x_min, x_max)
-    ax.set_xticks(x_ticks, x_ticks_names)
-    if savename == "":
-        y_min, y_max = np.min(datasets), np.max(datasets)
-    else:
-        y_min, y_max = 0, 1.4
-    ax.set_ylim(y_min - y_max/100, y_max + y_max/100)
-    ax.tick_params(axis = 'x', labelsize = 34)
-    ax.tick_params(axis = 'y', labelsize = 34)
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    legend = ax.legend(loc = "best",
-                    fontsize = 40)
-    plt.setp(legend.texts, family = "Times New Roman")
-    plt.tight_layout()
-    plt.savefig("plot_allofit" + savename + ".pdf",
-                dpi = my_dpi,
-                bbox_inches = "tight")
-    plt.close()
-
-
-######################################################
 # Hexagonal indexes to Miller indexes.
 # A, C, M, N, R.
 # (h, k, i, l)  ----> (h, k, l)
@@ -267,12 +203,12 @@ def compute_millers(peaks_flat, peaks_err_flat):
     # A: [2 -1 0] = 37.762°
     # C: [0 0 6] = 41.670°
     # M: [3 0 0] = 68.180°
-    # N: [1 1 3] = 43.5°
+    # N: [2 -1 3] = 43.338°
     # R: [1 0 -2] = 25.568°
     # R: [2 0 -4] = 52.534°
     # delta2theta = 0.1°
     ######################################################
-    Millers_exp = np.array([37.762, 41.670, 68.180, 43.5, 25.568, 52.534, 43.5])
+    Millers_exp = np.array([37.762, 41.670, 68.180, 43.338, 25.568, 52.534, 43.338])
     Millers_exp_err = 0.1
     Millers_exp_rel = np.max(Millers_exp_err / Millers_exp * 100)
     print("The max value for expected Millers relative error is {}%.".format(round(Millers_exp_rel, 4)))
@@ -284,7 +220,87 @@ def compute_millers(peaks_flat, peaks_err_flat):
             coincidence_checks += 1
     if coincidence_checks == len(Millers_exp):
         print("All discrepancies check out!")
+    indeces = np.array(["A", "C", "M", "N", "R", "R", "N"])
+    peaks_int_norm = np.array([1, 1, 1, 1, 1, 0.65, 0.8])
+    peaks_int = np.array([np.max(datasets[0][:, 1]), np.max(datasets[1][:, 1]), np.max(datasets[2][:, 1]), np.max(datasets[3][:, 1]), np.max(datasets[4][:, 1]), np.max(datasets[4][:, 1]) * 0.65, np.max(datasets[5][:, 1])])
 
+    return indeces, peaks_int_norm, peaks_int
+
+
+######################################################
+# Plotting for report.
+######################################################
+def plot_allofit(datasets, indeces, peaks, peaks_int, savename = ""):
+    my_dpi = 192
+    tnrfont = {'fontname':'Times New Roman'}
+    plt.rcParams['axes.axisbelow'] = True
+    plt.rcParams['mathtext.default'] = 'regular'
+    plt.rcParams["legend.frameon"] = True
+    plt.rcParams["legend.fancybox"] = False
+    zorders = np.linspace(1, 6, 6)
+    colors = ["orangered", "forestgreen", "lightseagreen", "darkorchid", "chartreuse", "crimson"]
+    labels = ["sample I", "sample II", "sample III", "sample IV", "sample V", "sample VI"]
+
+    _, ax = plt.subplots(num = "plotallofit",
+                            figsize = (4000/my_dpi, 2000/my_dpi),
+                            dpi = my_dpi)
+    for dataset in range(len(datasets)):
+        plt.plot(datasets[dataset][:, 0], datasets[dataset][:, 1],
+                color = colors[dataset],
+                linewidth = 1.5,
+                label = labels[dataset],
+                zorder = zorders[dataset])
+    for peak in range(len(peaks))[:-1]:
+        plt.annotate(indeces[peak],
+            xy = (peaks[peak] - 0.9, peaks_int[peak] + 0.01), xycoords = "data",
+            fontsize = 40)
+    plt.text(0.65, 0.9,
+            "Bragg-Brentano XRD diffractograms\nof single crystal sapphire thin films",
+            **tnrfont,
+            fontsize = 52,
+            horizontalalignment = 'center',
+            verticalalignment = 'center',
+            transform = ax.transAxes,
+            bbox = dict(facecolor = 'white', alpha = 1))
+    label_fontsize = 44
+    ax.set_xlabel("$2 \\vartheta$ ($deg$)",
+                fontsize = label_fontsize,
+                **tnrfont)
+    if savename == "":
+        ax.set_ylabel("counts",
+                fontsize = label_fontsize,
+                **tnrfont)
+    else:
+        ax.set_ylabel("normalized intensity (a.u.)",
+                fontsize = label_fontsize,
+                **tnrfont)
+    x_min, x_max = np.min(datasets[0][:, 0]), np.max(datasets[0][:, 0])
+    x_ticks = np.linspace(x_min, x_max, 10)
+    x_ticks_names = []
+    for tick in x_ticks:
+        x_ticks_names.append(str(int(tick)))
+    ax.set_xlim(6, 80)
+    ax.set_xticks(x_ticks, x_ticks_names)
+    if savename == "":
+        y_min, y_max = np.min(datasets), np.max(datasets)
+        ax.set_ylim(y_min - y_max/100, y_max + y_max/100 + 200)
+    else:
+        y_min, y_max = 0, 1
+        ax.set_ylim(y_min - y_max/100, 1.4)
+    y_ticks = np.linspace(y_min, y_max, 6)
+    ax.set_yticks(y_ticks)
+    ax.tick_params(axis = 'x', labelsize = 34)
+    ax.tick_params(axis = 'y', labelsize = 34)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    legend = ax.legend(loc = "best",
+                    fontsize = 40)
+    plt.setp(legend.texts, family = "Times New Roman")
+    plt.tight_layout()
+    plt.savefig("plot_allofit" + savename + ".pdf",
+                dpi = my_dpi,
+                bbox_inches = "tight")
+    plt.close()
 
 
 
@@ -295,8 +311,9 @@ def compute_millers(peaks_flat, peaks_err_flat):
 if __name__ == "__main__":
     # plot_everything(False)
     peaks_flat, peaks_err_flat = compute_peaks()
-    # plot_allofit(datasets)
-    # for dataset in range(len(datasets)):
-    #     datasets[dataset][:, 1] = datasets[dataset][:, 1] / np.max(datasets[dataset][:, 1])
-    # plot_allofit(datasets, savename = "_normalized")
-    compute_millers(peaks_flat, peaks_err_flat)
+    indeces, peaks_int_norm, peaks_int = compute_millers(peaks_flat, peaks_err_flat)
+    plot_allofit(datasets, indeces, peaks_flat, peaks_int)
+    for dataset in range(len(datasets)):
+        print(np.max(datasets[dataset][:, 1]))
+        datasets[dataset][:, 1] = datasets[dataset][:, 1] / np.max(datasets[dataset][:, 1])
+    plot_allofit(datasets, indeces, peaks_flat, peaks_int_norm, savename = "_normalized")
